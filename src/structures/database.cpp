67a6914668cd6411dcdcd768a6a7ce0824e9d4a0
DarioSindicic <dario.sindicic@gmail.com>
2020-01-19 22:03:01 +0100

    database impl

---
+++ src/structures/database.cpp
@@ -0,0 +1,18 @@
+#include "database.h"
+#include <stdexcept>
+#include <libpq-fe.h>
+
+Database::Database(char *ipAddress, int port) {
+    if(ipAddress == 0) {
+        throw std::invalid_argument("Ip address is pointing to zero.\n");
+    }
+    if(strlen(ipAddress) > MAX_LEN) {
+        throw std::invalid_argument("ipAddress is too long. It should be less than \n");
+    }
+    this->port = port;
+    strcpy(this->ipAddress, ipAddress);
+}
+
+bool Database::connect() {
+    return true;
+}

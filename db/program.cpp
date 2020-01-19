4d59af852c19c7608b0e9de3a9f77644bfe52ee1
DarioSindicic <dario.sindicic@gmail.com>
2020-01-19 15:14:14 +0100

    reading arguments and setting parameters

--- db/program.cpp
+++ db/program.cpp
@@ -1,11 +1,38 @@
 #include "program.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <syslog.h>
+#include <string.h>
 
 Program* Program::singleton = 0;
 
 Program::Program(){
     openlog("semantic-db", LOG_CONS | LOG_PID | LOG_NDELAY | LOG_NOWAIT, LOG_LOCAL1);
+}
+
+void Program::setUp(int argc, char* argv[]) {
+    int opt;
+    while( (opt = getopt(argc, argv, "c:l:")) != -1) {
+        
+        switch(opt) {
+            case 'c':
+                strcpy(config_file_path, optarg);
+                break;
+            case 'l':
+                log_level = atoi(optarg);
+                break;
+            default:
+                printf("Parameter %s is not acceptable.\n", optarg);
+                //usage();
+                return;
+        }
+    }
+
+}
+
+char* Program::getConfigFilePath() {
+    return this->config_file_path;
 }
 
 void Program::log(int priority, const char *format, ...) {
@@ -25,3 +52,6 @@ Program* Program::getInstance() {
     return singleton;
 }
 
+void Program::usage() {
+
+}

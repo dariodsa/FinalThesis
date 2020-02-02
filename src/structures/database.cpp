462e0214189337004e6c8ebc353e3ab58ba0fed0
DarioSindicic <dario.sindicic@gmail.com>
2020-02-02 18:04:40 +0100

    connecting and disconnecting to the base

--- src/structures/database.cpp
+++ src/structures/database.cpp
@@ -1,18 +1,46 @@
 #include "database.h"
+#include "program.h"
 #include <stdexcept>
 #include <libpq-fe.h>
 
-Database::Database(char *ipAddress, int port) {
+
+Database::Database(char *ipAddress, int port, char* username, char* password) {
     if(ipAddress == NULL) {
         throw std::invalid_argument("Ip address is pointing to zero.\n");
     }
     if(strlen(ipAddress) > MAX_LEN) {
         throw std::invalid_argument("ipAddress is too long. It should be less than \n");
     }
+
     this->port = port;
     strcpy(this->ipAddress, ipAddress);
 }
 
 bool Database::connect() {
+    Program* program = Program::getInstance();
+    try {
+        this->C = new connection("dbname = testdb user = postgres password = cohondob \
+            hostaddr = 127.0.0.1 port = 5432");
+        if (C->is_open()) {
+            return true;
+        } else {   
+            return false;
+        }
+    } catch (const std::exception &e) {
+        program->log(LOG_WARNING, "Exception message: %s", e.what());
+        return false;
+    }
+    return true;
+}
+
+bool Database::disconnect() {
+    Program* program = Program::getInstance();
+    try {
+        C->disconnect();
+    } catch(const std::exception &e) {
+        program->log(LOG_WARNING, "Exception message: %s", e.what());
+        return false;
+    }
+
     return true;
 }
--- src/structures/database.h
+++ src/structures/database.h
@@ -1,12 +1,22 @@
 #include <string.h>
+#include <pqxx/pqxx> 
+#include <stdio.h>
+
+using namespace pqxx;
 
 #define MAX_LEN 30
 
 class Database {
     public:
-        Database(char *ipAddress, int port);
+        Database(char *ipAddress, int port, char* username, char* password);
         bool connect();
+        bool disconnect();
     private:
     char ipAddress[MAX_LEN];
+    char username[MAX_LEN];
+    char password[MAX_LEN];
+
     int port;
+
+    pqxx::connection* C;
 };

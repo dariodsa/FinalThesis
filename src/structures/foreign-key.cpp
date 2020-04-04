a8770b9ad9af61c9b779449b9f7cdc021328aa04
DarioSindicic <dario.sindicic@gmail.com>
2020-04-04 18:06:32 +0200

    foreign key equal method based on rolling hash function, constant complexity

--- src/structures/foreign-key.cpp
+++ src/structures/foreign-key.cpp
@@ -9,15 +9,76 @@ ForeignKey::ForeignKey() {
 }
 void ForeignKey::setTable(const char *table1) {
     this->table1 = table1;
+    calculateHash1();
 }
 
 void ForeignKey::setTable2(const char *table2) {
     this->table2 = table2;
+    calculateHash3();
 }
 
 void ForeignKey::addColumnInTable1(char *column) {
     this->columns_table1.push_back(column);
+    calculateHash2();
 }
 void ForeignKey::addColumnInTable2(char *column) {
     this->columns_table2.push_back(column);
+    calculateHash4();
 } 
+
+void ForeignKey::calculateHash1() {
+    h1 = 0;
+    for(int i = 0, len = strlen(table1); i < len; ++i) {
+        h1 = h1*P1 + table1[i];
+    }
+}
+
+void ForeignKey::calculateHash2() {
+    h2 = 0;
+    for(char *col_name : columns_table1) {
+        for(int i = 0, len = strlen(col_name); i < len; ++i) {
+            h2 = h2*P2 + col_name[i];
+        }
+    }
+}
+
+void ForeignKey::calculateHash3() {
+    h3 = 0;
+    for(int i = 0, len = strlen(table2); i < len; ++i) {
+        h3 = h3*P3 + table2[i];
+    }
+}
+
+void ForeignKey::calculateHash4() {
+    h4 = 0;
+    for(char *col_name : columns_table2) {
+        for(int i = 0, len = strlen(col_name); i < len; ++i) {
+            h4 = h4*P4 + col_name[i];
+        }
+    }
+}
+
+int ForeignKey::getHash() {
+    hash = h1;
+    hash = hash * P + h2;
+    hash = hash * P + h3;
+    hash = hash * P + h4;
+
+    return this->hash;
+}
+bool ForeignKey::equalTo(ForeignKey* key) {
+    if(strcmp(table1, key->table1) != 0) return false;
+    if(strcmp(table2, key->table2) != 0) return false;
+    if(columns_table1.size() != key->columns_table1.size()) return false;
+    if(columns_table2.size() != key->columns_table2.size()) return false;
+
+    for(int i = 0, len = columns_table1.size(); i < len; ++i) {
+        if(strcmp(columns_table1[i], key->columns_table1[i]) != 0) return false;
+    }
+
+    for(int i = 0, len = columns_table2.size(); i < len; ++i) {
+        if(strcmp(columns_table2[i], key->columns_table2[i]) != 0) return false;
+    }
+
+    return true;
+}
--- src/structures/foreign-key.h
+++ src/structures/foreign-key.h
@@ -16,11 +16,30 @@ class ForeignKey{
     void setTable2(const char *table2);
     void addColumnInTable2(char* column);
 
+    int getHash();
+    bool equalTo(ForeignKey* key);
+
     private:
+
+    void calculateHash1();
+    void calculateHash2();
+    void calculateHash3();
+    void calculateHash4();
+
     const char* table1;
     const char* table2;
     std::vector<char*> columns_table1;
     std::vector<char*> columns_table2;
+
+    int hash;
+
+    int h1, h2, h3, h4;
+
+    const int P1 = 227;
+    const int P2 = 229; 
+    const int P3 = 233;
+    const int P4 = 239;
+    const int P = 241;
 };
 
 #endif

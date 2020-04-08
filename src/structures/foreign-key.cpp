763b8d778674283702ab116f9f80af7b4f119fa1
DarioSindicic <dario.sindicic@gmail.com>
2020-04-08 15:04:05 +0200

    foreign keys implementation

--- src/structures/foreign-key.cpp
+++ src/structures/foreign-key.cpp
@@ -2,10 +2,24 @@
 #include "table.h"
 #include "column.h"
 
+using namespace web;
 using namespace std;
 
 ForeignKey::ForeignKey() {
 
+}
+
+ForeignKey::ForeignKey(web::json::value json) {
+    setTable((char*)json["table1"].as_string().c_str());
+    setTable2((char*)json["table2"].as_string().c_str());
+    web::json::array col_tables1 = json["col_tables1"].as_array();
+    web::json::array col_tables2 = json["col_tables2"].as_array();
+    for(auto col : col_tables1) {
+        addColumnInTable1((char*)col.as_string().c_str());
+    }
+    for(auto col : col_tables2) {
+        addColumnInTable2((char*)col.as_string().c_str());
+    }
 }
 
 ForeignKey::ForeignKey(char* table1, vector<char*> columns1, char* table2, vector<char*> columns2) {
@@ -23,23 +37,42 @@ ForeignKey::ForeignKey(char* table1, vector<char*> columns1, char* table2, vecto
 }
 
 void ForeignKey::setTable(char *table1) {
+    for(int i = 0, len = strlen(table1); i < len; ++i) table1[i] = tolower(table1[i]);
     strcpy(this->table1, table1);
     calculateHash1();
 }
 
 void ForeignKey::setTable2(char *table2) {
+    for(int i = 0, len = strlen(table2); i < len; ++i) table2[i] = tolower(table2[i]);
     strcpy(this->table2, table2);
     calculateHash3();
 }
 
 void ForeignKey::addColumnInTable1(char *column) {
+    for(int i = 0, len = strlen(column); i < len; ++i) column[i] = tolower(column[i]);
     this->columns_table1.push_back(column);
     calculateHash2();
 }
 void ForeignKey::addColumnInTable2(char *column) {
+    for(int i = 0, len = strlen(column); i < len; ++i) column[i] = tolower(column[i]);
     this->columns_table2.push_back(column);
     calculateHash4();
 } 
+
+char* ForeignKey::getTable2() {
+    return this->table2;
+}
+
+char* ForeignKey::getTable1() {
+    return this->table1;
+}
+    
+vector<char*> ForeignKey::getColumns1() {
+    return this->columns_table1;
+}
+vector<char*> ForeignKey::getColumns2() {
+    return this->columns_table2;
+}
 
 void ForeignKey::calculateHash1() {
     h1 = 0;
@@ -96,4 +129,23 @@ bool ForeignKey::equalTo(ForeignKey* key) {
     }
 
     return true;
+}
+
+json::value ForeignKey::getJSON() {
+    json::value value;
+    value["table1"] = json::value::string(string(table1));
+    value["table2"] = json::value::string(string(table2));
+
+    value["col_tables1"] = json::value::array(columns_table1.size());
+    value["col_tables2"] = json::value::array(columns_table2.size());
+
+
+    for(int i = 0, len = columns_table1.size(); i < len; ++i) {
+        value["col_tables1"][i] = json::value::string(string(columns_table1[i]));
+    }
+    for(int i = 0, len = columns_table2.size(); i < len; ++i) {
+        value["col_tables2"][i] = json::value::string(string(columns_table2[i]));
+    }
+    
+    return value;
 }
--- src/structures/foreign-key.h
+++ src/structures/foreign-key.h
@@ -1,4 +1,9 @@
 #include <vector>
+#include <cpprest/json.h>
+
+#ifndef MAX_LEN
+#define MAX_LEN 50
+#endif
 
 #ifndef FOREIGN_KEY_H
 #define FOREIGN_KEY_H
@@ -9,6 +14,7 @@ class Column;
 class ForeignKey{
     public:
     ForeignKey();
+    ForeignKey(web::json::value);
     ForeignKey(char* table, std::vector<char*> columns_table1, char* table2, std::vector<char*> columns_table2);
     void setTable(char *table);
     void addColumnInTable1(char* column);
@@ -16,8 +22,16 @@ class ForeignKey{
     void setTable2(char *table2);
     void addColumnInTable2(char* column);
 
+    char* getTable1();
+    char* getTable2();
+    std::vector<char*> getColumns1();
+    std::vector<char*> getColumns2();
+    bool isIndex() { return false; }
+
     int getHash();
     bool equalTo(ForeignKey* key);
+
+    web::json::value getJSON();
 
     private:
 
@@ -26,8 +40,8 @@ class ForeignKey{
     void calculateHash3();
     void calculateHash4();
 
-    char* table1;
-    char* table2;
+    char table1[MAX_LEN];
+    char table2[MAX_LEN];
     std::vector<char*> columns_table1;
     std::vector<char*> columns_table2;
 

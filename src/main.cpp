cd6f30f3fc15ff60a5cdcd92333fedeb36ace4d3
DarioSindicic <dario.sindicic@gmail.com>
2020-04-13 22:29:21 +0200

    updated list of tpch queries, :egg:

--- src/main.cpp
+++ src/main.cpp
@@ -51,8 +51,10 @@ int main(int argc, char* argv[]) {
     
     //Proxy* p = new Proxy(55432, replicas[0]);
 
-    vector<const char*> queries;
-    for(int i=0;i<1;++i) {
+    vector<const char*> queries;    
+    
+    
+    for(int i=0;i<3;++i) {
         //queries.push_back("select * from sbtest1 as s1,sbtest2 as s2, sbtest3 as s3 where s1.k = s2.k + s3.k;");
         //query 2
         
@@ -69,12 +71,22 @@ int main(int argc, char* argv[]) {
         //6
         queries.push_back("select 	sum(l_extendedprice * l_discount) as revenue from 	lineitem where 	l_shipdate >= date '1994-01-01' 	and l_shipdate < date '1994-01-01' + interval '1' year 	and l_discount between .06 - 0.01 and .06 + 0.01 	and l_quantity < 24; ");
         
+        //7
+        queries.push_back("select 	supp_nation, 	cust_nation, 	l_year, 	sum(volume) as revenue from 	( 		select 			n1.n_name , 			n2.n_name, 			extract(year from l_shipdate) , 			l_extendedprice * (1 - l_discount) 		from 			supplier, 			lineitem, 			orders, 			customer, 			nation n1, 			nation n2 		where 			s_suppkey = l_suppkey 			and o_orderkey = l_orderkey 			and c_custkey = o_custkey 			and s_nationkey = n1.n_nationkey 			and c_nationkey = n2.n_nationkey 			and ( 				(n1.n_name = 'FRANCE' and n2.n_name = 'GERMANY') 				or (n1.n_name = 'GERMANY' and n2.n_name = 'FRANCE') 			) 			and l_shipdate between date '1995-01-01' and date '1996-12-31' 	)  group by 	supp_nation, 	cust_nation, 	l_year order by 	supp_nation, 	cust_nation, 	l_year;");
+
+        //8
+        queries.push_back("select 	o_year, 	sum(case 		when nation = 'BRAZIL' then volume 		else 0 	end) / sum(volume) as mkt_share from 	( 		select 			extract(year from o_orderdate) as o_year, 			l_extendedprice * (1 - l_discount) as volume, 			n2.n_name as nation 		from 			part, 			supplier, 			lineitem, 			orders, 			customer, 			nation n1, 			nation n2, 			region 		where 			p_partkey = l_partkey 			and s_suppkey = l_suppkey 			and l_orderkey = o_orderkey 			and o_custkey = c_custkey 			and c_nationkey = n1.n_nationkey 			and n1.n_regionkey = r_regionkey 			and r_name = 'AMERICA' 			and s_nationkey = n2.n_nationkey 			and o_orderdate between date '1995-01-01' and date '1996-12-31' 			and p_type = 'ECONOMY ANODIZED STEEL' 	)  group by 	o_year order by 	o_year;");
+
+        //9
+        queries.push_back("select 	nation, 	o_year, 	sum(amount) as sum_profit from 	( 		select 			n_name as nation, 			extract(year from o_orderdate) as o_year, 			l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount 		from 			part, 			supplier, 			lineitem, 			partsupp, 			orders, 			nation 		where 			s_suppkey = l_suppkey 			and ps_suppkey = l_suppkey 			and ps_partkey = l_partkey 			and p_partkey = l_partkey 			and o_orderkey = l_orderkey 			and s_nationkey = n_nationkey 			and p_name like '%green%' 	) group by 	nation, 	o_year order by 	nation, 	o_year desc;");
 
         //10
         queries.push_back("select 	c_custkey, 	c_name, 	sum(l_extendedprice * (1 - l_discount)) as revenue, 	c_acctbal, 	n_name, 	c_address, 	c_phone, 	c_comment from 	customer, 	orders, 	lineitem, 	nation where    c_custkey = o_custkey 	and l_orderkey = o_orderkey 	and o_orderdate >= date '1993-10-01' 	and o_orderdate < date '1993-10-01' + interval '3' month 	and l_returnflag = 'R' 	and c_nationkey = n_nationkey group by 	c_custkey, 	c_name, 	c_acctbal, 	c_phone, 	n_name, 	c_address, 	c_comment order by 	revenue desc;");
         
         //11
         queries.push_back("select 	ps_partkey, 	sum(ps_supplycost * ps_availqty) as value from 	partsupp, 	supplier, 	nation where 	ps_suppkey = s_suppkey 	and s_nationkey = n_nationkey 	and n_name = 'GERMANY' group by 	ps_partkey having 		sum(ps_supplycost * ps_availqty) > ( 			select 				sum(ps_supplycost * ps_availqty) * 0.0001000000 			from 				partsupp, 				supplier, 				nation 			where 				ps_suppkey = s_suppkey 				and s_nationkey = n_nationkey 				and n_name = 'GERMANY' 		) order by 	value desc;");
+        queries.push_back("select sum(ps_supplycost * ps_availqty) * 0.0001000000 from partsupp, supplier, nation where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY';");
+        
 
         //12
         queries.push_back("select 	l_shipmode, 	sum(case 		when o_orderpriority = '1-URGENT' 			or o_orderpriority = '2-HIGH' 			then 1 		else 0 	end) as high_line_count, 	sum(case 		when o_orderpriority <> '1-URGENT' 			and o_orderpriority <> '2-HIGH' 			then 1 		else 0 	end) as low_line_count from 	orders, 	lineitem where 	o_orderkey = l_orderkey 	and l_shipmode in ('MAIL', 'SHIP') 	and l_commitdate < l_receiptdate 	and l_shipdate < l_commitdate 	and l_receiptdate >= date '1994-01-01' 	and l_receiptdate < date '1994-01-01' + interval '1' year group by 	l_shipmode order by 	l_shipmode;");
@@ -99,6 +111,16 @@ int main(int argc, char* argv[]) {
         //18
         queries.push_back("select 	c_name, 	c_custkey, 	o_orderkey, 	o_orderdate, 	o_totalprice, 	sum(l_quantity) from 	customer, 	orders, 	lineitem where 	o_orderkey in ( 		select 			l_orderkey 		from 			lineitem 		group by 			l_orderkey having 				sum(l_quantity) > 300 	) 	and c_custkey = o_custkey 	and o_orderkey = l_orderkey group by 	c_name, 	c_custkey, 	o_orderkey, 	o_orderdate, 	o_totalprice order by 	o_totalprice desc, 	o_orderdate;");
         
+        //19
+        queries.push_back("select 	sum(l_extendedprice* (1 - l_discount)) as revenue from 	lineitem, 	part where 	( 		p_partkey = l_partkey 		and p_brand = 'Brand#12' 		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG') 		and l_quantity >= 1 and l_quantity <= 1 + 10 		and p_size between 1 and 5 		and l_shipmode in ('AIR', 'AIR REG') 		and l_shipinstruct = 'DELIVER IN PERSON' 	) 	or 	( 		p_partkey = l_partkey 		and p_brand = 'Brand#23' 		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK') 		and l_quantity >= 10 and l_quantity <= 10 + 10 		and p_size between 1 and 10 		and l_shipmode in ('AIR', 'AIR REG') 		and l_shipinstruct = 'DELIVER IN PERSON' 	) 	or 	( 		p_partkey = l_partkey 		and p_brand = 'Brand#34' 		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG') 		and l_quantity >= 20 and l_quantity <= 20 + 10 		and p_size between 1 and 15 		and l_shipmode in ('AIR', 'AIR REG') 		and l_shipinstruct = 'DELIVER IN PERSON' 	);");
+        
+        //20
+        queries.push_back("select 	s_name, 	s_address from 	supplier, 	nation where 	s_suppkey in ( 		select 			ps_suppkey 		from 			partsupp 		where 			ps_partkey in ( 				select 					p_partkey 				from 					part 				where 					p_name like 'forest%' 			) 			and ps_availqty > ( 				select 					0.5 * sum(l_quantity) 				from 					lineitem 				where 					l_partkey = ps_partkey 					and l_suppkey = ps_suppkey 					and l_shipdate >= date '1994-01-01' 					and l_shipdate < date '1994-01-01' + interval '1' year 			) 	) 	and s_nationkey = n_nationkey 	and n_name = 'CANADA' order by 	s_name;");
+
+        //21
+        //queries.push_back("select 	s_name, 	count(*) as numwait from 	supplier, 	lineitem l1, 	orders, 	nation where 	s_suppkey = l1.l_suppkey 	and o_orderkey = l1.l_orderkey 	and o_orderstatus = 'F' 	and l1.l_receiptdate > l1.l_commitdate 	and exists ( 		select 			* 		from 			lineitem l2 		where 			l2.l_orderkey = l1.l_orderkey 			and l2.l_suppkey <> l1.l_suppkey 	) 	and not exists ( 		select 			* 		from 			lineitem l3 		where 			l3.l_orderkey = l1.l_orderkey 			and l3.l_suppkey <> l1.l_suppkey 			and l3.l_receiptdate > l3.l_commitdate 	) 	and s_nationkey = n_nationkey 	and n_name = 'SAUDI ARABIA' group by 	s_name order by 	numwait desc, 	s_name; ");
+    
+
         //queries.push_back("select count(*) from region, nation where r_regionkey = n_regionkey;");
         
         //1
